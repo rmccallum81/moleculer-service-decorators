5cf4a838041955785809ab8e8d759af0dead94c1
rmccallum81 <rmccallum81@gmail.com>
2018-05-17 11:01:01 -0400

    Lifecycle events

--- src/event.ts
+++ src/event.ts
@@ -6,16 +6,21 @@ import {
     setMetadata,
 } from "./utils/metadata";
 
-export interface EventOptions {
-    name?: ServiceEvent["name"];
-    group?: ServiceEvent["group"];
-}
-
+/**
+ * Options for the event, based off ServiceEvent
+ */
+export type EventOptions = Partial<Pick<ServiceEvent,
+                                Exclude<keyof ServiceEvent,
+                                    "handler">>>;
+/**
+ * Add metod as a service event handler
+ * @param {EventOptions} options
+ */
 export function event(options?: EventOptions): MethodDecorator {
     return <T>(target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<T>) => {
         const handler = descriptor.value;
         const eventName: string = (options || {}).name || propertyKey.toString();
-        if (handler && isFunction(handler)) {
+        if (isFunction(handler)) {
             const opts: EventOptions = { name: propertyKey.toString(), ...options };
             const events = getMetadata(target, "events") || {};
 
@@ -26,8 +31,54 @@ export function event(options?: EventOptions): MethodDecorator {
             setMetadata(target, "events", events);
 
             return descriptor;
-        } else {
-            throw new TypeError("An event handler must be a function/method");
         }
+        throw new TypeError("An event handler must be a function/method");
     };
+}
+
+/**
+ * Available lifecycle events
+ */
+export type LifeCycleEventNames = "created" | "started" | "stopped";
+
+/**
+ * Add method as a lifecycle event handler
+ * @param name Name of the lifecycle event
+ */
+export function lifeCycleEvent(name: LifeCycleEventNames): MethodDecorator {
+    if (!name) {
+        throw new ReferenceError("Lifecycle event name required");
+    }
+
+    return <T>(target: Object, _propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<T>) => {
+        const handler = descriptor.value;
+        if (isFunction(handler)) {
+            setMetadata(target, name, handler);
+
+            return descriptor;
+        }
+
+        throw new TypeError("A lifecycle event handler must be a function/method");
+    };
+}
+
+/**
+ * Service created event
+ */
+export function serviceCreated(): MethodDecorator {
+    return lifeCycleEvent("created");
+}
+
+/**
+ * Service started event
+ */
+export function serviceStarted(): MethodDecorator {
+    return lifeCycleEvent("started");
+}
+
+/**
+ * Service stopped event
+ */
+export function serviceStopped(): MethodDecorator {
+    return lifeCycleEvent("stopped");
 }
